f780336c82b5706481c873b4fbd87029053cace2
Icebearbear <yuliatiyuli39@gmail.com>
2021-05-20 17:04:31 +0800

    use controlled components and keep data in array explained in README under what is happening in controlled comp

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # Learning-React
+
+## Running
+
+1. go to path directory
+2. npm start
+
+## controlled components
+a component that its state is controlled by other components, it doesn't have a state
+
+
+<input> maintain its own state and update its state with user input.
+1. the state is updated with setState
+2. the display value is this.state.value
+3. the input's value is driven by React state
+4. it can be passed to another UI or component
+5. setState is to re-render the component
+
+what is really happening:
+1. when user gives input, onChange will call the callback function handleChange
+2. in callback function, create another state variable 
+3. once submit button is clicked, onClick will call the callback function handleSubmit
+4.  in callback function, it reference the input element, get its current value, and    update it with the state variable created at no.2
+
--- my-shopping/src/index.js
+++ my-shopping/src/index.js
@@ -2,25 +2,41 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Info extends React.Component{
+// Info component is a controlled components
+ class Info extends React.Component{
   render(){
     return(
-      <h1>{this.props.value}</h1>
+      <div>
+      <ol> 
+        {this.props.value.map((item, index) => {
+        return <li key={`${index}${item}`}>{item}</li> // set item and index as key
+      })}
+      </ol>
+      </div>
     )
   }
 }
+
 class Tab extends React.Component{
   constructor(props){
     super(props);
-    this.state = { valueQ : ''}; // declare state initial value called valueQ to be maintained by component 
-    this.handleChange = this.handleChange.bind(this); // bind(this) is to pass this and retain its value as instances of component
-    this.handleSubmit = this.handleSubmit.bind(this);
+    this.state = { 
+      valueQ : ['Sally', 'Icebear']}; // declare state initial value called valueQ to be maintained by component 
+    // this.handleChange = this.handleChange.bind(this); // bind(this) is to pass this and retain its value as instances of component
+    // this.handleSubmit = this.handleSubmit.bind(this);
   }
-  handleChange(event){ // event handler
-    this.setState({valueQ: event.target.value}) // re-render when there is a change in valueQ
+  handleChange = (e) => { // event handler
+    // set input to the current value
+    this.setState({ input: e.target.value}) // re-render when there is a change in valueQ
   }
-  handleSubmit(){
-    alert('submitted ' + this.state.valueQ)
+  handleSubmit = () => {
+    // alert('submit' + this.state.valueQ)
+
+    // concat input into valueQ, it will create new array for immutability
+    // setState the newest valueQ, it is a async means the state is not updated instantly
+    // use snapshot of previous state to create a new state to combine diff state tgt
+    // and to avoid side effect of async
+    this.setState(prevState => ({valueQ: this.state.valueQ.concat(this.state.input)}))
   }
   
   render(){
@@ -28,13 +44,13 @@ class Tab extends React.Component{
       <div>
           <label>
             Item:
-            <textarea type="text" value={this.state.valueQ} onChange={this.handleChange}></textarea>
+            <textarea type="text" onChange={this.handleChange}></textarea>
           </label>
           <button onClick= {this.handleSubmit}>
             submit
           </button>
 
-        <Info value={this.state.valueQ}/>
+          <Info value={this.state.valueQ}/>    
       </div>
     )
   }
