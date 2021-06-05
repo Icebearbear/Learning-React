a40eece1d05c01d5810a08026f3224ae5e57424f
Icebearbear <yuliatiyuli39@gmail.com>
2021-06-06 01:27:49 +0800

    created firebase project and installed express 1.  installed firebase CLI 2.  creted firebase project and used firebase-function 3.  created a new project in firebase store 4.  installed express and cors inside firebase project 5.  might be replacing the previous server with this one

---
+++ servers/functions/index.js
@@ -0,0 +1,36 @@
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+const express = require('express');
+const cors = require('cors');
+const app = express();
+app.use(cors({ origin: true }));
+
+// initialize firebase and get firestore
+var serviceAccount = require("./permission_key.json"); // service account private key
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://todolist2-b7979..firebaseio.com"
+});
+const db = admin.firestore();
+
+// return hello world
+app.get('/hello-world', (req, res) => {
+  return res.status(200).send('Hello World!');
+});
+
+
+// create to db
+app.post('/api/create', (req, res) => {
+    (async () => {
+        try {
+          await db.collection('items').doc('/' + req.body.id + '/')
+              .create({item: req.body.item});
+          return res.status(200).send();
+        } catch (error) {
+          console.log(error);
+          return res.status(500).send(error);
+        }
+      })();
+  });
+
+exports.app = functions.https.onRequest(app);
